11bd90d5df45cbbaa9f62d542ff069636ded5a1e
Philip Rideout <philiprideout@gmail.com>
2018-11-18 17:22:25 -0800

    Progress

--- materials/build.sh
+++ materials/build.sh
@@ -4,6 +4,8 @@ set -e
 FILAMENT_REPO=~/github/filament
 FILAMENT_OUT=$FILAMENT_REPO/out/cmake-release
 MATC=$FILAMENT_OUT/tools/matc/matc
+MATINFO=$FILAMENT_OUT/tools/matinfo/matinfo
 MATC_FLAGS='-O -a opengl -m material -p mobile'
 
 $MATC $MATC_FLAGS -o tracks.filamat tracks.mat
+echo $MATINFO -g 1 tracks.filamat
