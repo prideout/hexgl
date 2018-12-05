8af33eab4186614d19b3427466bf208397afd25f
Philip Rideout <philiprideout@gmail.com>
2018-12-05 09:44:52 -0800

    Add OBJ and filamesh.

---
+++ convert_models.sh
@@ -0,0 +1,5 @@
+export filamesh=~/github/filament/out/cmake-release/tools/filamesh/filamesh
+${filamesh} -m tracks.obj    tracks/filamesh
+${filamesh} -m scrapers1.obj scrapers1/filamesh
+${filamesh} -m scrapers2.obj scrapers2/filamesh
+${filamesh} -m ship.obj      ship/filamesh
