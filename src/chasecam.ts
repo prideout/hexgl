f386c6b04f95eb60644ffb799587ead247e208ab
Philip Rideout <philiprideout@gmail.com>
2018-12-17 00:03:36 +0100

    Tweak ChaseCam.

--- src/chasecam.ts
+++ src/chasecam.ts
@@ -18,48 +18,47 @@ export default class ChaseCamera {
     private readonly vehicle: Vehicle;
     private readonly speedOffsetMax: number;
     private readonly yoffset: number;
+    private readonly targetYoffset: number;
     private readonly zoffset: number;
-    private readonly viewOffset: number;
 
     private speedOffset: number;
 
     constructor(camera: Filament.Camera, vehicle: Vehicle) {
         this.camera = camera;
         this.vehicle = vehicle;
-        this.speedOffset = 0;
+        this.yoffset = 10.0; // how far above the ship the camera flies
+        this.zoffset = 23.0; // how far behind the ship the camera flies
+        this.speedOffset = 0; // adds to zoffset after sudden acceleration
         this.speedOffsetMax = 10;
-        this.yoffset = 8.0;
-        this.zoffset = 30.0; // <= this was 10 in the original game
-        this.viewOffset = 10.0;
+
+        this.targetYoffset = 6.0; // how far above the ship the camera looks
     }
 
     public tick(dt: number, speed: number) {
+        this.speedOffset += (this.speedOffsetMax * speed - this.speedOffset) *
+                Math.min(1.0, 0.3 * dt);
+
         const up = vec3.fromValues(0, 1, 0);
         vec3.transformQuat(up, up, this.vehicle.orientation);
 
         const dir = vec3.fromValues(0, 0, 1);
         vec3.transformQuat(dir, dir, this.vehicle.orientation);
 
-        this.speedOffset += (this.speedOffsetMax * speed - this.speedOffset) *
-                Math.min(1.0, 0.3 * dt);
-
-        const position = vec3.copy(vec3.create(), this.vehicle.position);
-
+        const cameraPos = vec3.copy(vec3.create(), this.vehicle.position);
         vec3.scale(dir, dir, this.zoffset + this.speedOffset);
-        vec3.subtract(position, position, dir);
-
+        vec3.subtract(cameraPos, cameraPos, dir);
         vec3.scale(up, up, this.yoffset);
-        vec3.add(position, position, up);
+        vec3.add(cameraPos, cameraPos, up);
 
-        position[1] += -up[1] + this.yoffset;
-
-        vec3.normalize(dir, dir);
-        vec3.scale(dir, dir, this.viewOffset);
-        const target = vec3.add(vec3.create(), position, dir);
+        const targetPos = vec3.copy(vec3.create(), this.vehicle.position);
+        vec3.set(up, 0, 1, 0);
+        vec3.transformQuat(up, up, this.vehicle.orientation);
+        vec3.scale(up, up, this.targetYoffset);
+        vec3.add(targetPos, targetPos, up);
 
         this.camera.lookAt(
-                position as unknown as number[],
-                target as unknown as number[],
+                cameraPos as unknown as number[],
+                targetPos as unknown as number[],
                 [0, 1, 0]);
     }
 }
