0ca949e9f7e3bffa0149591fcc2a8ae9ab12c8a6
Philip Rideout <philiprideout@gmail.com>
2018-12-17 00:46:27 +0100

    webpack now includes the WASM loader

--- update_filament.sh
+++ update_filament.sh
@@ -1,2 +1,3 @@
-cp ../filament/out/cmake-webgl-release/web/filament-js/filament.js .
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.js ../hexgl_source/src/
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.d.js ../hexgl_source/src/
 cp ../filament/out/cmake-webgl-release/web/filament-js/filament.wasm .
