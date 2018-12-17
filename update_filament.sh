a6a25122a209ff7edefc600b8c6928888d807da8
Philip Rideout <philiprideout@gmail.com>
2018-12-17 15:22:35 +0100

    Add fork ribbon and instructions.

--- update_filament.sh
+++ update_filament.sh
@@ -1,3 +1,4 @@
-cp ../filament/out/cmake-webgl-release/web/filament-js/filament.js ../hexgl_source/src/
-cp ../filament/out/cmake-webgl-release/web/filament-js/filament.d.js ../hexgl_source/src/
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.js ../hexgl_source/filament/
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.d.ts ../hexgl_source/filament/
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.wasm ../hexgl_source/filament/
 cp ../filament/out/cmake-webgl-release/web/filament-js/filament.wasm .
